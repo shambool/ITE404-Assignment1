4796c9570356b7d776f4862d41a7f05ac08f5e7e
Yaduh <yadhawar2003@gmail.com>
2025-11-06 15:35:05 +0300

    wa

--- test.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-app.listen(8081, () => console.log('Server running on http://localhost:8081'));
